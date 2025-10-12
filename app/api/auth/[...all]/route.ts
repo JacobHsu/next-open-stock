2f409bcf5e28c6597bbeb99faeb494fd838afe2d
jacobhsu <jacob.hsu.tw@gmail.com>
2025-10-12 20:45:11 +0800

    chore: Failed to collect page data for /api/auth/

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,6 +1,9 @@
 import { getAuth } from "@/lib/better-auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+
 export const GET = async (req: Request) => {
     const auth = await getAuth();
     return toNextJsHandler(auth)(req);
